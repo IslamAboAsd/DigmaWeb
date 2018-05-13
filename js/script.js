4fabf36d71ea2166340df0330582aff2f4d7b4b2
eslam200788 <eslam200788@gmail.com>
2018-05-13 14:12:11 +0200

    add Nice scroll

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 (function($) {
 	
 	"use strict";
+
 	
+ // Add smooth scrolling to all links
+$(".navbar-nav>li>a").on('click', function(event) {
+
+	// Make sure this.hash has a value before overriding default behavior
+	if (this.hash !== "") {
+	  // Prevent default anchor click behavior
+	  event.preventDefault();
+  
+	  // Store hash
+	  var hash = this.hash;
+	  window.location.hash = hash;
+  
+	  // Using jQuery's animate() method to add smooth page scroll
+	  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	  $('html, body').animate({
+		scrollTop: $(hash).offset().top - 120
+	  }, 1000, function(){
+		  $('.navbar-collapse').removeClass('in');
+		// Add hash (#) to URL when done scrolling (default click behavior)
+	  });
+	} // End if
+  });
 	//Hide Loading Box (Preloader)
 	function handlePreloader() {
 		if($('.preloader').length){
@@ -412,5 +435,8 @@
         transitionStyle:"goDown",
         navigationText:["",""],
         autoPlay:false
-    });
+	});
+
+	// triger nicescroll
+	$('html').niceScroll();
 })(window.jQuery);
